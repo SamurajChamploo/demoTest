57fee0916bf749457486c0fa8fb1a4b0fe1607ec
justinadashkevich <purityknight99@gmail.com>
2024-04-10 23:50:01 +0300

    wednesday

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+    @GetMapping(value="/hello")
+    public String test() {
+        return "Hello world";
+    }
+    @GetMapping(value="/bye")
+    public String testik() {
+        return "Bye world";
+    }
+}
