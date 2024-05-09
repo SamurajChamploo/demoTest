7d4f6dce8a34464de015a97ea8c99e23e4985143
justinadashkevich <purityknight99@gmail.com>
2024-05-09 16:16:35 +0300

    es-40: create tale Managers, entity and repository

---
+++ src/main/java/com/example/demo/domain/Managers.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "managers")
+@ToString
+@Getter
+public class Managers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "first_name_ru", nullable = false)
+    private String firstNameRu;
+    @Column(name = "last_name_ru", nullable = false)
+    private String lastNameRu;
+    @Column(name = "middle_name_ru", nullable = false)
+    private String middleNameRu;
+    @Column(name = "first_name_en", nullable = false)
+    private String firstNameEn;
+    @Column(name = "last_name_en", nullable = false)
+    private String lastNameEn;
+    @Column(name = "middle_name_en", nullable = false)
+    private String middleNameEn;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "time_zone")
+    private String timeZone;
+}
