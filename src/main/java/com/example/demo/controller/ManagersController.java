639dca5014c2e9523fb5f4a8c5ca8854040aa562
justinadashkevich <purityknight99@gmail.com>
2024-06-14 17:31:07 +0300

    ES-53: adding logging in CustomersController, CustomersService with lombok, ManagersController, ManagersService with regular logger

--- src/main/java/com/example/demo/controller/ManagersController.java
+++ src/main/java/com/example/demo/controller/ManagersController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = "/managers")
 public class ManagersController {
     private final ManagersService managersService;
+
+    private static Logger log = Logger.getLogger(ManagersController.class.getName());
 
     public ManagersController(ManagersService managersService) {
         this.managersService = managersService;
@@ -18,15 +21,18 @@ public class ManagersController {
 
     @GetMapping(value = "/byFirstNameEnAndLastNameEn")
     public String getManagerByFirstNameEnAndLastNameEn(String firstNameEn, String lastNameEn) {
+        log.info("Getting manager's phone number by its firstNameEn and lastNameEn");
         return managersService.getManagerPhoneNumberByFirstNameEnAndLastNameEn(firstNameEn, lastNameEn);
     }
     @GetMapping
     public List<String> getManagers() {
+        log.info("Getting a list of managers' phone numbers");
         return managersService.getManagers();
     }
 
     @GetMapping(value = "/byTimeZone")
     public String getManagerByTimeZone(String timeZone) {
+        log.info("Getting manager's phone number by their time zone");
         return managersService.getManagersByTimeZone(timeZone);
     }
 }
--- src/main/java/com/example/demo/service/ManagersService.java
+++ src/main/java/com/example/demo/service/ManagersService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class ManagersService {
@@ -15,6 +16,8 @@ public class ManagersService {
 
     private final ManagersRepository managersRepository;
 
+    private static Logger log = Logger.getLogger(ManagersService.class.getName());
+
     public ManagersService(ManagersRepository managersRepository) {
         this.managersRepository = managersRepository;
     }
@@ -22,34 +25,42 @@ public class ManagersService {
     public String getManagerPhoneNumberByFirstNameEnAndLastNameEn(String firstNameEn, String lastNameEn) {
         Managers manager = managersRepository.findTopByFirstNameEnAndLastNameEn(firstNameEn, lastNameEn);
         if (manager != null) {
+            log.info("Getting manager's phone number by their firstNameEn and lastNameEn");
             return manager.getPhoneNumber();
         }
-        return "Нет менеджера по вашим критериям?";
+        log.info("No manager by such firstNameEn and lastNameEn");
+        return "Нет менеджера по вашим критериям";
     }
 
     public List<String> getManagers() {
         List<Managers> managers = managersRepository.findAll();
         if (CollectionUtils.isEmpty(managers)) {
+            log.info("No managers in DB");
             return List.of("Нет менеджеров в базе");
         }
+        log.info("Getting a list of managers' phone numbers");
         return managers.stream()
                 .map(Managers::getPhoneNumber)
                 .toList();
     }
 
     public String getManagersByTimeZone(String timeZone) {
-        Managers manager = managersRepository.findTopByTimeZone(timeZone);
+        Managers manager = managersRepository.findTopByTimeZone(timeZone); // не находит никого, всегда null возвращает
         if (manager == null) {
+            log.info("Getting default Moscow manager");
             return getMoscowManager();
         }
+        log.info("Getting manager's phone number by their time zone");
         return manager.getPhoneNumber();
     }
 
     private String getMoscowManager() {
         Managers managerMoscow = managersRepository.findTopByTimeZone(moscowTimeZone);
         if (managerMoscow == null) {
+            log.info("No managers in DB at all");
             return "Нет вообще менеджеров";
         }
+        log.info("Getting Moscow manager's phone number");
         return managerMoscow.getPhoneNumber();
     }
 }
